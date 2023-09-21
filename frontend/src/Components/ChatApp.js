d238e957a72006198cb1075c4e43e0bc5875644d
nitinrajputind <nitinrajput971625@gmail.com>
2023-09-21 19:06:12 +0530

    Done

--- frontend/src/Components/ChatApp.js
+++ frontend/src/Components/ChatApp.js
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from "react";
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:4000/");
+
+const socket = io("http://localhost:4000/")
+// const socket = io("https://chatapp-bw3a.onrender.com/");
 
 const userName = prompt("Enter your name");
 
